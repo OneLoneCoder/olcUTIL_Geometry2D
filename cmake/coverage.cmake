4ad6f7abd19b14daa4c245c7d6ef10916f4bb852
Martin Kielhorn <wolpumba@gmail.com>
2024-02-05 21:14:23 +0100

    is branch coverage working on ubuntu now?

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -24,16 +24,19 @@ function(add_coverage_target exclude)
 
         # lcov on ubuntu 22 doesn't support --branch-coverage
         set(COV_OPT 
+        ""  # i modify /etc/lcovrc in the github action
             #"--branch-coverage"
-            "--rc lcov_branch_coverage=1"
+            #"--rc lcov_branch_coverage=1" # this doesn't work with -l option
         )
+        # option for genhtml --rc genhtml_branch_coverage=1
+
         add_custom_command(
             OUTPUT  ${covname}
-            COMMAND ${LCOV} -c -o ${covname} ${COV_OPT} -d . -b . --gcov-tool ${GCOV} --ignore-errors mismatch
+            COMMAND ${LCOV} -c -o ${covname} ${COV_OPT} -d . -b . --gcov-tool ${GCOV} #--ignore-errors mismatch
             # In the following I explicitly exclude system headers from the coverage report
             COMMAND ${LCOV} -r ${covname} ${COV_OPT}  -o ${covname} ${exclude}  "\*/googletest/\*" "\*/g++\*/bits/\*" "'*/g++-v13/*'"
             COMMAND ${LCOV} -l ${covname} ${COV_OPT} 
-            COMMAND ${GENHTML} ${covname} --branch-coverage -output coverage
+            COMMAND ${GENHTML} ${covname} --rc lcov_branch_coverage=1 -output coverage
             COMMAND ${LCOV} -l ${covname} ${COV_OPT}  2>/dev/null | grep Total | sed 's/|//g' | sed 's/Total://g' | awk '{print $1}' | sed s/%//g > coverage/total
         )
         set_directory_properties(PROPERTIES
